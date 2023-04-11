b17d220fa1c93cceb968ff6ac7677d00c6e2c065
remybms <remy.bamas@ynov.com>
2023-04-11 08:55:22 +0200

    delete useless function

--- pun/components/game.js
+++
@@ -1,8 +0,0 @@
-function counter() {
-    var fs = require('fs');
-    fs.readdir('/magasins', function (error, files) {
-        var totalFiles = files.length; // return the number of files
-        console.log(totalFiles); // print the total number of files 
-    })
-}
-export default counter

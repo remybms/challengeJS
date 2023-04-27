fd7243410b991bbfe5c96eee1e6429986180485f
remybms <remy.bamas@ynov.com>
2023-04-27 11:19:54 +0200

    change rules

--- pun/pages/index.js
+++ pun/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       <main className={styles.main}>
           <h1>Bienvenue dans PUN !</h1>
           <div>Votre objectif dans ce jeu est de retrouver les jeux de mots de nos amis les petits commerçants.</div>
-          <div>Chaque partie comporte 5 manches de 30 secondes chacunes.</div>
+          <div>Chaque partie comporte 3 manches de 30 secondes chacunes.</div>
           <div>
             <div>3 niveaux de difficultés sont disponibles :</div>
             <div>Facile : vous aurez à l'écran la devanture du magasin avec son nom en partie caché ainsi qu'un petit indice sur le nom à trouver</div>

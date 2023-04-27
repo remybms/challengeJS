972a4fb8864b8ac9c746522ca95f0abec4072ce6
remybms <remy.bamas@ynov.com>
2023-04-27 08:41:57 +0200

    fix functions's issues

--- pun/functions/game.js
+++ pun/functions/game.js
@@ -4,34 +4,45 @@ export function getImageRandom() {
 
 export function timer(start, timeLeft) {
     if (typeof document !== 'undefined') {
-        document.getElementById("gameStart").addEventListener("click", function () {
-
-            start(true)
-            var sec = 15
-            var timer = setInterval(function () {
-                if (typeof document !== 'undefined') {
-                    document.getElementById('safeTimerDisplay').innerHTML = sec;
-                    sec--;
-
-                    if (sec < 0) {
-                        clearInterval(timer)
-                        timeLeft(false)
-                    }
+        start(true)
+        var sec = 15
+        var timer = setInterval(function () {
+            if (typeof document !== 'undefined') {
+                if (document.getElementById('safeTimerDisplay') != null) {
+                    document.getElementById('safeTimerDisplay').textContent = sec;
                 }
+                sec--;
 
-
-            }, 1000)
-
-        })
-
+                if (sec < 0) {
+                    clearInterval(timer)
+                    timeLeft(false)
+                }
+            }
+        }, 1000)
     }
 }
 
 export function isAnswer(value, timeLeft, answer, URLanswer) {
-    let reader = new FileReader()
-    console.log(reader.readAsText(URLanswer))
-    timeLeft = false
-    if (value === reader.readAsText(URLanswer)){
-        answer = true
-    }
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', URLanswer);
+    xhr.responseType = 'blob';
+    timeLeft(false)
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            const reader = new FileReader();
+            reader.onload = function () {
+                console.log(answer)
+                if (answer == reader.result){
+                    value(true)
+                }
+            };
+            reader.readAsText(xhr.response);
+        } else {
+            console.error('Erreur de chargement du fichier: ', xhr.statusText);
+        }
+    };
+
+    xhr.send();
 }
+
+

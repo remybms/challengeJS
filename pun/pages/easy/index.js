e35eb42f775c444ef4b0b5d0918fce2c96095cf5
remybms <remy.bamas@ynov.com>
2023-05-09 08:23:15 +0200

    edit pages

--- pun/pages/easy/index.js
+++ pun/pages/easy/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import React from 'react'
 import { useState } from 'react';
-import { timer, isAnswer, setPoints, Points, nextRound, readFileContent } from '@/functions/game';
+import { timer, isAnswer, setPoints, Points, nextRound, displayIndice } from '@/functions/game';
 import styles from "../../styles/levels.module.css"
+import Link from 'next/link';
 
 
 export default function Easy() {
@@ -16,12 +17,14 @@ export default function Easy() {
 
     const [image, setImage] = useState(Number)
 
+    const [round, setRound] = useState(true)
+
     const level = "easy"
     const src = '/magasins/' + image + '.png'
     const srcHalfBlurred = '/titleHalfBlurred/' + image + '.png'
     const indice = '/indices/' + image + '.txt'
     const answer = '/answers/' + image + '.txt'
-    
+
 
     return (
         <><Head>
@@ -29,49 +32,68 @@ export default function Easy() {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
-            {gameStarted ? (
-                <>
-                    {timeLeft ? (<>
-                        <audio controls autoPlay hidden>
-                            <source src='/sound/level_music.mp3'></source>
-                        </audio>
-                        <h1 id='safeTimerDisplay'></h1>
-                        <div>
-                           <img src={srcHalfBlurred} id='imageBlurred' className={styles.image}></img>
-                           <div>{readFileContent(indice)}</div>
-                        </div>
-                        
-                        <form>
-                            <input type='text' id='textAnswer' onChange={(e) => { setAnswer(e.target.value) }} />
-                        </form>
+            <div className={styles.page}>
+                {round ? (<>
+                    {gameStarted ? (
+                        <>
+                            {timeLeft ? (<>
+                                <audio controls autoPlay hidden>
+                                    <source src='/sound/level_music.mp3'></source>
+                                </audio>
+                                <h1 id='safeTimerDisplay' className={styles.title}></h1>
+                                <div className={styles.divImage}>
+                                    <img src={srcHalfBlurred} id='imageBlurred' className={styles.image} onLoad={() => displayIndice(indice)}></img>
+                                </div>
+                                <div id='indice' className={styles.text}></div>
+                                <form className={styles.form}>
+                                    <input type='text' id='textAnswer' onChange={(e) => { setAnswer(e.target.value) }} className={styles.input} />
+                                </form>
 
-                        <button id='submitAnswer' onClick={() => isAnswer(guessedAnswer, noTimeLeft, checkAnswer, answer)}>envoyer</button>
+                                <button id='button' onClick={() => isAnswer(guessedAnswer, noTimeLeft, checkAnswer, answer)} className={styles.button}>envoyer</button>
 
-                    </>) : (<>
-                        {goodAnswer ? (<>
-                        <h1> TEMPS ÉCOULÉ ! Bien joué champion</h1>
-                        <img src={src} id='image' className={styles.image}></img>
-                        <div>{guessedAnswer}</div>
-                        {setPoints(level)}
-                        <div>Vous avez au total {Points} point(s)</div>
-                        <button id='submitAnswer' onClick={() => nextRound(updateGameStarted, noTimeLeft, checkAnswer, setAnswer)}>Prochaine manche</button>
+                            </>) : (<>
+                                {goodAnswer ? (<>
+                                    <audio controls autoPlay hidden>
+                                        <source src='/sound/ending_round_win.mp3'></source>
+                                    </audio>
+                                    <h1 className={styles.title}> TEMPS ÉCOULÉ ! Bien joué champion</h1>
+                                    <div className={styles.divImage}>
+                                        <img src={src} id='image' className={styles.image} onLoad={() => setPoints(level)}></img>
+                                    </div>
+                                    <div className={styles.text}>Vous avez au total {Points} point(s)</div>
+                                    <button id='button' onClick={() => nextRound(updateGameStarted, noTimeLeft, checkAnswer, setAnswer, setRound)} className={styles.button}>Prochaine manche</button>
+                                </>
+                                ) : (<>
+                                    <audio controls autoPlay hidden>
+                                        <source src='/sound/ending_round_lose.mp3'></source>
+                                    </audio>
+                                    <h1 className={styles.title}> TEMPS ÉCOULÉ ! Une prochaine fois</h1>
+                                    <div className={styles.divImage}>
+                                        <img src={src} id='image' className={styles.image}></img>
+                                    </div>
+
+                                    <button id='button' onClick={() => nextRound(updateGameStarted, noTimeLeft, checkAnswer, setAnswer, setRound)} className={styles.button}>Prochaine manche</button>
+                                </>
+                                )}
+
+
+
+                            </>)}
                         </>
-                        ) : (<> 
-                        <h1> TEMPS ÉCOULÉ ! Une prochaine fois</h1>
-                        <img src={src} id='image' className={styles.image}></img>
-                        <div>{guessedAnswer}</div>
-                        <button id='submitAnswer' onClick={() => nextRound(updateGameStarted, noTimeLeft, checkAnswer, setAnswer)}>Prochaine manche</button>
+                    ) : (
+                        <>
+                            <h1 className={styles.title}>Vous êtes prêt ?</h1>
+                            <button id='button' onClick={() => timer(updateGameStarted, noTimeLeft, setImage)} className={styles.buttonStart}>launch the game</button>
                         </>
-                        )}
-                        
-                        
+                    )}
+                </>
+                ) : (
+                    <>
+                        <Link href='/result' className={styles.title}>Voir les résultats</Link>
+                    </>
+                )}
 
-                    </>)}
-                </>
-            ) : (
-                <>
-                    <button id='gameStart' onClick={() => timer(updateGameStarted, noTimeLeft, setImage)} >launch the game</button>
-                </>
-            )}
+            </div>
+
         </>)
 }

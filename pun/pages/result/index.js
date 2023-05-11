4b20b67564f60282b75a5b4415216e76e9512a07
remybms <remy.bamas@ynov.com>
2023-05-11 09:45:47 +0200

    ending result page

--- pun/pages/result/index.js
+++ pun/pages/result/index.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Points } from '@/functions/levels';
 import styles from '../../styles/result.module.css'
 import { setResult } from '@/functions/results';
+import Link from 'next/link'
 
 export default function result() {
 
@@ -26,6 +27,7 @@ export default function result() {
                     </audio>
                     <h1 className={styles.text}>T'es vraiment mauvais</h1>
                     <div className={styles.points}>0 pointé, t'aurais pas pu faire pire</div>
+                    <Link href='/' className={styles.points}>Retourner au menu principal</Link>
                 </>) : (<>
                     {fullPoints ? (<>
                         <audio controls autoPlay hidden>
@@ -33,12 +35,14 @@ export default function result() {
                         </audio>
                         <h1 className={styles.text}>Un niveau impresionnant</h1>
                         <div className={styles.points}>T'aurais pas pu faire mieux, 9 points</div>
+                        <Link href='/' className={styles.points}>Retourner au menu principal</Link>
                     </>) : (<>
                         <audio controls autoPlay hidden>
                             <source src={src}></source>
                         </audio>
                         <h1 className={styles.text}>pas si mal</h1>
                         <div className={styles.points}>tu as {Points} point(s)</div>
+                        <Link href='/' className={styles.points}>Retourner au menu principal</Link>
                     </>)}
                 </>)}
             </>

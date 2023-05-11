327c708420223daf733b3ff59876adf328aa594e
remybms <remy.bamas@ynov.com>
2023-05-11 09:02:30 +0200

    create result page

--- pun/pages/result/index.js
+++ pun/pages/result/index.js
@@ -1,38 +1,47 @@
 import Head from 'next/head';
 import { useState } from 'react';
 import { Points } from '@/functions/levels';
+import styles from '../../styles/result.module.css'
+import { setResult } from '@/functions/results';
 
 export default function result() {
 
     const [noPoints, isPoints] = useState(false)
     const [fullPoints, isFullPoints] = useState(false)
-
-    return (
-        <>
-            <Head>
-                <title>Terminé</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            {noPoints ? (<>
-                <audio controls autoPlay hidden>
-                    <source src='/sound/ending_game_0points.mp3'></source>
-                </audio>
-                <h1>t'es vraiment mauvais</h1>
-            </>) : (<>
-                {fullPoints ? (<>
+    const [src, setSrc] = useState(String)
+    if (src == "") {
+        setResult(isPoints, isFullPoints, Points, setSrc)
+    } else {
+        return (
+            <>
+                <Head>
+                    <title>Terminé</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <img src='/clown.jpg' className={styles.background} />
+                {noPoints ? (<>
                     <audio controls autoPlay hidden>
-                        <source src='/sound/ending_game_max_points.mp3'></source>
+                        <source src={src}></source>
                     </audio>
-                    <h1>un niveau impresionnant</h1>
+                    <h1 className={styles.text}>T'es vraiment mauvais</h1>
+                    <div className={styles.points}>0 pointé, t'aurais pas pu faire pire</div>
                 </>) : (<>
-                    <audio controls autoPlay hidden>
-                        <source src='/sound/ending_game.mp3'></source>
-                    </audio>
-                    <h1>pas si mal</h1>
-                    <div>tu as {Points} points</div>
+                    {fullPoints ? (<>
+                        <audio controls autoPlay hidden>
+                            <source src={src}></source>
+                        </audio>
+                        <h1 className={styles.text}>Un niveau impresionnant</h1>
+                        <div className={styles.points}>T'aurais pas pu faire mieux, 9 points</div>
+                    </>) : (<>
+                        <audio controls autoPlay hidden>
+                            <source src={src}></source>
+                        </audio>
+                        <h1 className={styles.text}>pas si mal</h1>
+                        <div className={styles.points}>tu as {Points} point(s)</div>
+                    </>)}
                 </>)}
-            </>)}
-        </>
-    )
+            </>
+        )
+    }
 }

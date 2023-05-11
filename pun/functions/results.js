09a9a677016aec360c9aa72b9f418939397137bc
remybms <remy.bamas@ynov.com>
2023-05-11 09:00:56 +0200

    edit result function to defined sound src

--- pun/functions/results.js
+++ pun/functions/results.js
@@ -1,7 +1,11 @@
-export function setResult(noPoints, fullPoints, points){
+export function setResult(noPoints, fullPoints, points, src){
     if (points == 0){
         noPoints(true)
+        src('/sound/ending_game_0points.mp3')
     } else if (points == 9){
         fullPoints(true)
+        src('/sound/ending_game_max_points.mp3')
+    } else {
+        src('/sound/ending_game.mp3')
     }
 }
